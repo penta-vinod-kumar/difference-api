6da761c108509906724c431ce7f15f0eea01ea31
Penta, Vinod Kumar (ITCDEC) - KLM <vinod.penta@klm.com>
2020-09-23 21:20:45 +0200

    initial commit with springboot stater

---
+++ src/main/java/com/waes/vinod/differenceapi/DifferenceApiApplication.java
@@ -0,0 +1,13 @@
+package com.waes.vinod.differenceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DifferenceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DifferenceApiApplication.class, args);
+	}
+
+}
